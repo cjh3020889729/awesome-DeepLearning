d0c4a12b709271f7a91fbf0f6079974e24a9b60e
Jinghui Cai <55022974+cjh3020889729@users.noreply.github.com>
2021-11-10 21:42:45 +0800

    Update point.py

--- Paddle_Enterprise_CaseBook/robot_grab/code/point.py
+++ Paddle_Enterprise_CaseBook/robot_grab/code/point.py
@@ -6,8 +6,8 @@ from point_tools import parse_mask_edge_points, visualize_mask_edge
 
 model = paddlex.load_model('output/mask_rcnn_r50_fpn/best_model')
 
-img = cv2.imread('dataset/JPEGImages/Image_20210615204254171.bmp')
-result = model.predict('dataset/JPEGImages/Image_20210615204254171.bmp', transforms=model.test_transforms)
+img = cv2.imread('dataset/JPEGImages/Image_20210615204210757.bmp')
+result = model.predict('dataset/JPEGImages/Image_20210615204210757.bmp', transforms=model.test_transforms)
 
 mask_edge_points = parse_mask_edge_points(result, score_threshold=0.95)
 img = visualize_mask_edge(img, mask_edge_points=mask_edge_points, point_size=1, color=(0,0,255))
